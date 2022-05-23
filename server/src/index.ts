5f4e748825c38a20257e3c5929ee7fb1700b67d1
Gerset Linarez <58746126+RedCode26@users.noreply.github.com>
2022-05-23 02:28:33 -0500

    Apollo client config

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,15 @@
 const { ApolloServer } = require('apollo-server');
 const typeDefs = require("./graphql/typeDefs");
 import resolvers from "./graphql/resolvers/";
-const PORT = 8000;
+import Dotenv from "dotenv";
+Dotenv.config()
+const PORT = process.env.PORT || 8000;
 
 // The ApolloServer constructor requires two parameters: your schema
 // definition and your set of resolvers.
 const server = new ApolloServer({ typeDefs, resolvers });
 
 // The `listen` method launches a web server.
-server.listen().then(({ url }: any) => {
+server.listen(PORT).then(({ url }: any) => {
     console.log(`Server ready at ${url}`);
 });
