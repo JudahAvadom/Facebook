f2306ffa03ee2932a177c3373793bc77e87076b1
Gerset Linarez <58746126+RedCode26@users.noreply.github.com>
2022-05-23 02:16:35 -0500

    React Setup

--- web/webpack.config.js
+++ web/webpack.config.js
@@ -1,15 +1,22 @@
 const path = require('path');
 const HTMLWebpackPlugin = require( 'html-webpack-plugin' );
+const MiniCssExtractPlugin = require( 'mini-css-extract-plugin' );
+const WebpackMessages = require('webpack-messages');
 const Dotenv = require('dotenv');
 Dotenv.config();
 
 let mode = process.env.NODE_ENV || "development";
 let port = process.env.PORT || 8000;
+let target = 'web';
+
+if (process.env.NODE_ENV === "production") {
+    target = "browserslist";
+}
 
 module.exports = {
     mode,
-    target: 'web',
-    entry: ['./src/index.js'],
+    target,
+    entry: ['./src/index.jsx'],
     output: {
         path: path.resolve( __dirname, 'dist' ),
         filename: 'build/[name].js',
@@ -20,7 +27,27 @@ module.exports = {
                 test: /\.jsx?$/,
                 exclude: /node_modules/,
                 use: [ 'babel-loader' ]
-            }
+            },
+            {
+                test: /\.(s[ac]|c)ss$/i,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                    },
+                    "css-loader",
+                    "postcss-loader",
+                    "sass-loader",
+                ],
+            },
+            {
+                test: /\.svg$/,
+                use: [
+                    {
+                        loader: 'svg-url-loader',
+                        options: {limit: 10000,},
+                    },
+                ],
+            },
         ]
     },
     plugins: [
@@ -29,6 +56,13 @@ module.exports = {
             template: path.resolve( __dirname, 'index.html' ),
             minify: false,
         }),
+        new MiniCssExtractPlugin({
+            filename: 'build/styles.css'
+        }),
+        new WebpackMessages({
+            name: 'client',
+            logger: str => console.log(`>> ${str}`)
+        })
     ],
     resolve: {
         modules: [path.join(__dirname, 'src'), 'node_modules'],
@@ -37,7 +71,10 @@ module.exports = {
     devServer: {
         port,
         historyApiFallback: true,
-        allowedHosts: ["all"]
+        static: path.join(__dirname, 'dev'),
+        allowedHosts: ["all"],
+        hot: true,
     },
+    stats: "errors-only",
     devtool: 'source-map'
 }
