64bacf07c835134326b0a5446a9deb016b841bee
Gerset Linarez <58746126+RedCode26@users.noreply.github.com>
2022-05-23 09:16:42 -0500

    Create ButtoLink component

--- web/src/Components/util/Butto.tsx
+++ web/src/Components/util/Butto.tsx
@@ -1,11 +1,21 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import './styles.css'
 
-const Butto = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+const Button = ({ variant = 'primary', children, ...props }: any) => {
+    return (
+        <button className={`button button--${variant}`} {...props}>
+            {children}
+        </button>
+    )
 }
 
-export default Butto
+const ButtonLink = ({ variant = 'primary', children, ...props }: any) => {
+    return (
+        <Link className={`button-link button-link--${variant}`} {...props}>
+            {children}
+        </Link>
+    )
+}
+
+export { Button, ButtonLink }
