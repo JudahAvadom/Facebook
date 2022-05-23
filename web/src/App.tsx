779ce9a7e890447e1065c22ac0a7a98286862712
Gerset Linarez <58746126+RedCode26@users.noreply.github.com>
2022-05-23 07:11:03 -0500

    Create page Home

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,7 +1,13 @@
 import React from 'react'
+import Home from './Pages/Home'
+import './scss/index.scss'
 
 const App = () => {
-  return <div>Hola</div>
+    return (
+        <div>
+            <Home />
+        </div>
+    )
 }
 
 export default App
---
+++ web/src/Pages/Home.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div className="home__wrapper">
+            <h1 className="home__title">Facebook Clone</h1>
+        </div>
+    )
+}
+
+export default Home
