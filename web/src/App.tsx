92c0790d2352bc970d1f15fa7c2b7fef4fca2a3a
Gerset Linarez <58746126+RedCode26@users.noreply.github.com>
2022-05-23 07:17:49 -0500

    Set initial route

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,12 +1,15 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './Pages/Home'
 import './scss/index.scss'
 
 const App = () => {
     return (
-        <div>
-            <Home />
-        </div>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />} />
+            </Routes>
+        </BrowserRouter>
     )
 }
 
