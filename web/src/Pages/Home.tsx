27ed6527beeb58849895bb205183eb9f61e98c11
Gerset Linarez <58746126+RedCode26@users.noreply.github.com>
2022-05-24 10:56:15 -0500

    use footer test

--- web/src/Pages/Home.tsx
+++ web/src/Pages/Home.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import LoginForm from '../Components/Auth/LoginForm'
+import Footer from '../Components/util/Fooiter'
 
 const Home = () => {
     return (
         <div className="home__wrapper">
-            <LoginForm />
+            <section className="home__section">
+                <LoginForm />
+            </section>
+            <Footer />
         </div>
     )
 }
