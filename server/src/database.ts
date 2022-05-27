8a60c1b0e8afcc936f760c8913fe876e19e9ba75
Gerset Linarez <58746126+RedCode26@users.noreply.github.com>
2022-05-26 23:39:54 -0500

    database

---
+++ server/src/database.ts
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+import Dotenv from "dotenv";
+Dotenv.config();
+
+const MONGO_URI = process.env.MONGODB || 'mongodb://localhost:27017/facebookdb';
+
+export const Connection = async() => {
+    await mongoose.connect(MONGO_URI, {useNewUrlParser: true})
+    
+}
